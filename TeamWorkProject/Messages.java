0ef78388dbd18e21b42dea9ded3fb48448429565
YNikolov <yavor_nikolov@abv.bg>
2014-06-06 20:58:19 +0300

    the game BulsAndCows

---
+++ TeamWorkProject/Messages.java
@@ -0,0 +1,19 @@
+public class Messages
+    {
+        public static String RecconnectRequest = "rcn";
+        public static String RequestUserName = "unm";
+        public static String RequestToPlay = "rtp";
+        public static String AcceptTeamPlay = "atp";
+        public static String StartTeamPlay = "stp";
+        public static String DeclineTeamPlay = "dtp";
+        public static String RespondToRequestFirst = "rtr";
+        public static String RespondUserToPlayWith = "rupw";
+        public static String QuitMultiplayer = "qmp";
+        public static String OponentNotAvailable = "ona";
+        public static String GameEnded = "gen";
+        public static String ReportClients = "rcl";
+        public static String OponentDisconnected = "opd";
+        public static String UnknownCommand = "unk";
+        public static String AwaitingCommand = "awc";
+        public static String Dice = "dic";
+    }                 
