e577d03c9d65ae0ccc0c0ce0dec677f032d7dc3d
YNikolov <yavor_nikolov@abv.bg>
2015-06-18 00:34:38 +0300

    Deleted unused lines

--- PersonalProjects/ASP.NET/ASP.NET Web API/MusicArtists/MusicArtists.Data/Migrations/Configuration.cs
+++ PersonalProjects/ASP.NET/ASP.NET Web API/MusicArtists/MusicArtists.Data/Migrations/Configuration.cs
@@ -22,140 +22,101 @@ namespace MusicArtists.Data.Migrations
 
         protected override void Seed(MusicArtistsDbContext context)
         {
-           // this.SeedArtists(context);
+            this.SeedArtists(context);
             //this.SeedAlbums(context);
             //this.SeedSongs(context);
         }
 
         private void SeedArtists(IMusicArtistsDbContext context)
         {
-            //if (context.Artists.Any())
-            //{
-            //    return;
-            //}
-            //var christina = new Artist()
-            //{
-            //    Name = "Christina Aguilera",
-            //    Country = "USA",
-            //    //BirthDay = new DateTime(2008, 3, 15)
-            //};
-            //christina.Albums = new List<Album>()
-            //{
-            //   new Album() 
-            //   {
-            //        Name = "Dirrty",
-            //        Genre = "Pop",
-            //        Producer = "Christina Aguilera",
-            //        //Year = DateTime.Parse("09/19/2013 00:29 AM", new System.Globalization.CultureInfo("en-US", false)),
-            //        Songs = new List<Song>()
-            //        {
-            //            new Song()
-            //            {
-            //                Genre = "Pop",
-            //                Name = "Dirrty",
-            //                Artist = "Christina Aguilera"
-            //            }
-            //        }
-            //   },
-            //   new Album() 
-            //   {
-            //        Name = "Stripped",
-            //        Genre = "Pop",
-            //        Producer = "Christina Aguilera",
-            //        //Year = DateTime.Parse("09/19/2013 00:29 AM", new System.Globalization.CultureInfo("en-US", false)),
-            //        Songs = new List<Song>()
-            //        {
-            //            new Song()
-            //            {
-            //                Genre = "Pop",
-            //                Name = "Stripped",
-            //                Artist = "Christina Aguilera"
-            //            },
-            //            new Song()
-            //            {
-            //                Genre = "Pop",
-            //                Name = "Fighter",
-            //                Artist = "Christina Aguilera"
-            //            },
-            //            new Song()
-            //            {
-            //                Genre = "Pop",
-            //                Name = "Beautyfull",
-            //                Artist = "Christina Aguilera"
-            //            }
-            //        }
-            //   },
-            //   new Album() 
-            //   {
-            //        Name = "BackToBasics",
-            //        Genre = "Pop",
-            //        Producer = "Christina Aguilera",
-            //        //Year = new DateTime(2013,12,1),
-            //        Songs = new List<Song>()
-            //        {
-            //            new Song()
-            //            {
-            //                Genre = "Pop",
-            //                Name = "Back",
-            //                Artist = "Christina Aguilera",
-
-            //            },
-            //            new Song()
-            //            {
-            //                Genre = "Pop",
-            //                Name = "Basics",
-            //                Artist = "Christina Aguilera",
-            //                //Year = new DateTime(2013,12,1)
-            //            },
-            //            new Song()
-            //            {
-            //                Genre = "Pop",
-            //                Name = "Stronger",
-            //                Artist = "Christina Aguilera",
-            //                //Year = new DateTime(2013,12,1)
-            //            },
-            //            new Song()
-            //            {
-            //                Genre = "Pop",
-            //                Name = "AddOrUpdateTest",
-            //                Artist = "Christina Aguilera",
-            //                //Year = new DateTime(2013,12,1)
-            //            }
-            //        }
-            //   }            
-            //};
-            //var testDateTime = new Artist()
-            //{
-            //    Name = "Test BirthDay Format",
-            //    Country = "USA",
-            //    //BirthDay = new DateTime(2008, 3, 15)
-            //};
-            //var test = new Artist()
-            //{
-            //    Name = "latest",
-            //    Country = "Test"
-            //};
-            //            Това ли искаш
-            //string dateString;
-            //        DateTime result;
-            //        CultureInfo provider = CultureInfo.InvariantCulture;
-
-            //        dateString = "01 01 2015";
-            //        string[] format = new string[] { "dd/M/yyyy", "dd mm yyyy" };
-            //        try
-            //        {
-            //            result = DateTime.ParseExact(dateString, format, provider, DateTimeStyles.None);
-            //            Console.WriteLine("{0} converts to {1}.", dateString, result.ToString());
-            //        }
-            //        catch (FormatException)
-            //        {
-            //            Console.WriteLine("{0} is not in the correct format.", dateString);
-            //        }
+            if (context.Artists.Any())
+            {
+                return;
+            }
+            var christina = new Artist()
+            {
+                Name = "Christina Aguilera",
+                Country = "USA",
+                
+            };
+            christina.Albums = new List<Album>()
+            {
+               new Album() 
+               {
+                    Name = "Dirrty",
+                    Genre = "Pop",
+                    Producer = "Christina Aguilera",                    
+                    Songs = new List<Song>()
+                    {
+                        new Song()
+                        {
+                            Genre = "Pop",
+                            Name = "Dirrty"                            
+                        }
+                    }
+               },
+               new Album() 
+               {
+                    Name = "Stripped",
+                    Genre = "Pop",
+                    Producer = "Christina Aguilera",                    
+                    Songs = new List<Song>()
+                    {
+                        new Song()
+                        {
+                            Genre = "Pop",
+                            Name = "Stripped"                            
+                        },
+                        new Song()
+                        {
+                            Genre = "Pop",
+                            Name = "Fighter"                            
+                        },
+                        new Song()
+                        {
+                            Genre = "Pop",
+                            Name = "Beautyfull"
+                        }
+                    }
+               },
+               new Album() 
+               {
+                    Name = "BackToBasics",
+                    Genre = "Pop",
+                    Producer = "Christina Aguilera",
+                    Songs = new List<Song>()
+                    {
+                        new Song()
+                        {
+                            Genre = "Pop",
+                            Name = "Back"
+                        },
+                        new Song()
+                        {
+                            Genre = "Pop",
+                            Name = "Basics"
+                        },
+                        new Song()
+                        {
+                            Genre = "Pop",
+                            Name = "Stronger"
+                        },
+                        new Song()
+                        {
+                            Genre = "Pop",
+                            Name = "AddOrUpdateTest"
+                        }
+                    }
+               }            
+            };
+            
+                        
             // TODO add singers INNA
-            //context.Artists.Add(testDateTime);
-            //context.Artists.Add(christina);
+            
+            
+            context.Artists.Add(christina);
             //context.Artists.Add(adam);
-            //context.SaveChanges();
+            context.SaveChanges();
             }
         }
     }
