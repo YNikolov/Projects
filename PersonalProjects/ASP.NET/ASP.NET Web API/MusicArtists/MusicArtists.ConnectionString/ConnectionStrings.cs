72da48a807e05202ef15e1765c17ecf9a95f6aa9
YNikolov <yavor_nikolov@abv.bg>
2015-06-17 23:47:41 +0300

    Added Connection String

---
+++ PersonalProjects/ASP.NET/ASP.NET Web API/MusicArtists/MusicArtists.ConnectionString/ConnectionStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicArtists.ConnectionString
+{
+    public static class ConnectionStrings
+    {
+        public const string DefaultConnection = @"Data Source=Y\YAV;Initial Catalog=MusicArtist;Integrated Security=True";
+    }
+}
